40431e12bcc4ac8831f3becce7d28bee97ad30b6
suttoko <suttoko37@gmial.com>
2026-03-02 22:02:12 +0900

    テスト

--- fuel/app/views/menu.php
+++ fuel/app/views/menu.php
@@ -13,7 +13,7 @@
 	<section class="f-sokuhimeContents <?php echo 'aaad'; ?>">
 		<h2 class="sokuhimeContents-head"><span class="sokuhimeContents-ttl"><em
 					class="sokuhimeContents-area"><?php echo $area; ?></em></span>
-			<span class="sokuhimeContens-update">更新</span>
+			<span class="sokuhimeContens-update">更sss新</span>
 		</h2>
 	</section>
 	<p>menu</p>
